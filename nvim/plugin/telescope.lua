83b7751a543ddfccde8c98aa5fcfeb3f71e20288
Tyler Cunnion <tyler.cunnion@gmail.com>
2026-04-16 01:59:04 +0000

    Don't load telescope-fzf in minimal profile

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -1,14 +1,22 @@
-require("telescope").setup({
-	extensions = {
+local profile = require("config.profile")
+
+local telescope_opts = {}
+if profile.full then
+	telescope_opts.extensions = {
 		fzf = {
 			fuzzy = true,
 			override_generic_sorter = true,
 			override_file_sorter = true,
 			case_mode = "smart_case",
 		},
-	},
-})
-require("telescope").load_extension("fzf")
+	}
+end
+
+require("telescope").setup(telescope_opts)
+
+if profile.full then
+	require("telescope").load_extension("fzf")
+end
 
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
